e35b5fff05ba2c0d2585d9ebec00e76466086e74
luzelenie <luzelenie1205@gmail.com>
2021-08-11 00:12:03 -0400

    commit tree 726db052e36951d39acfa30c6baf89108098add2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+
+function superbowlWin(array) {
+let win = null
+ if ( win = array.find(element => element.result === "W"))
+  {     return win.year
+    } 
+    else return undefined
+    }
+
+superbowlWin(record)
